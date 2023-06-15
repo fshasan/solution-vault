0dadb3c9f13a141da4a6e7988f8a2acd993b24c4
Farhan Sharukh Hasan <sharukh75hasan@gmail.com>
2023-06-16 01:03:43 +0600

    Update 1572. Matrix Diagonal Sum.py

--- Algorithms/1572. Matrix Diagonal Sum.py
+++ Algorithms/1572. Matrix Diagonal Sum.py
@@ -3,8 +3,6 @@ class Solution(object):
         sum = 0
         for i in range(len(mat)):
             for j in range(len(mat)):
-                if i == j:
-                    sum += mat[i][j]
-                elif j == len(mat) - i - 1:
+                if i == j or j == len(mat) - i - 1:
                     sum += mat[i][j]
         return sum
