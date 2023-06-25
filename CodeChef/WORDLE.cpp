3a9f29e942716ed4968280e7dc4f7e33ef143581
Farhan Sharukh Hasan <sharukh75hasan@gmail.com>
2023-06-26 02:24:19 +0600

    Add files via upload

---
+++ CodeChef/WORDLE.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    char S[5], T[5], M[5];
+	cin >> n;
+	
+	while(n != 0){
+	    for(int i = 0; i < 5; i++){
+	        cin >> S[i];
+	    }
+	    for(int i = 0; i < 5; i++){
+	        cin >> T[i];
+            if (S[i] == T[i]){
+                M[i] = 'G';
+            }else{
+                M[i] = 'B';
+            }
+	    }
+	    
+	    for(int i = 0; i < 5; i++){
+	        cout << M[i];
+	    }
+	    
+	    cout << '\n';
+	    
+	    n--;
+	}
+	
+	return 0;
+}
