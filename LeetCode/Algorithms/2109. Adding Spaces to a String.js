a93013d16ee05ddbfa99d9d5d7eb6dac8f2eccee
Farhan Sharukh Hasan <sharukh75hasan@gmail.com>
2023-12-26 15:15:49 +0600

    Create 2109. Adding Spaces to a String.js

---
+++ LeetCode/Algorithms/2109. Adding Spaces to a String.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {number[]} spaces
+ * @return {string}
+ */
+var addSpaces = function(s, spaces) {
+        let i = 0;
+        let j = 0;
+        let ans = '';
+
+        while (i < s.length) {
+            if (j > spaces.length - 1) {
+                
+            } else if (i === spaces[j]) {
+                ans += ' ';
+                j += 1;
+            }
+            ans += s[i];
+            i += 1;
+        }
+
+        return ans;
+};
